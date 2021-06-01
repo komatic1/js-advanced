6c95fdef2caf7a0f034826a090cc4a1cc1e4949d
Dima Dmitriev <komatic.dda@gmail.com>
2021-06-01 18:10:28 +0300

    03-project
    download file byt create a tag
    
    --- TASK

--- 03_Project/src/js/main.js
+++ 03_Project/src/js/main.js
@@ -5,6 +5,7 @@ import Difference from './modules/difference';
 //import Form from './modules/forms';
 import showInfo from './modules/showInfo';
 import ShowInfo from './modules/showInfo';
+import Download from './modules/download';
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new MainSlider({btns: '.next', container: '.page'});
@@ -49,4 +50,6 @@ window.addEventListener('DOMContentLoaded', () => {
     bigSlider.render();
 
     new ShowInfo('.plus__content').init();
+
+    new Download('.download').init();
 });
